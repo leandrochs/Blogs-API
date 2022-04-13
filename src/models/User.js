9a392680a774a4d61dd411fe9a4786a1b10cf4b2
leandrochs <leandrochagask@gmail.com>
2022-04-13 12:20:00 -0300

    feat: insere associate com blogPost

--- src/models/User.js
+++ src/models/User.js
@@ -2,18 +2,20 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
-      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      displayName: DataTypes.STRING,
+      displayName: { type: DataTypes.STRING, allowNull: false },
       email: { type: DataTypes.STRING, isEmail: true, unique: true },
-      password: DataTypes.STRING,
+      password: { type: DataTypes.STRING, allowNull: false },
       image: { type: DataTypes.STRING, isUrl: true },
     },
-    {
-      timestamps: false,
-      tableName: 'Users',
-      // underscored: true,
-    },
+    { timestamps: false, tableName: 'Users' },
   );
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      as: 'blogPosts',
+      foreignKey: 'userId',
+    });
+  };
 
   return User;
 };
