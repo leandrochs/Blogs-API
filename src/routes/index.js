daf082083e3d98708b521cfca96db0d90205fd9a
leandrochs <leandrochagask@gmail.com>
2022-04-13 12:22:17 -0300

    feat: insere index de para rotas categories e blogPost

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,4 @@
 module.exports.userRoutes = require('./userRoutes');
 module.exports.loginRoutes = require('./loginRoutes');
+module.exports.categoriesRoutes = require('./categoriesRoutes');
+module.exports.blogPostRoutes = require('./blogPostRoutes');
