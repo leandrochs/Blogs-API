33ec425c30cd3e69a97c19bc26437e97ffd53f2d
leandrochs <leandrochagask@gmail.com>
2022-04-13 12:11:27 -0300

    feat: insere rotas para categories e post

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const { userRoutes, loginRoutes } = require('./src/routes');
+const { userRoutes, loginRoutes, categoriesRoutes, blogPostRoutes } = require('./src/routes');
 
 app.use('/user', userRoutes);
 app.use('/login', loginRoutes);
+app.use('/categories', categoriesRoutes);
+app.use('/post', blogPostRoutes);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
